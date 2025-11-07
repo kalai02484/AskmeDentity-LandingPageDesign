ae758ff7628c909585f344c6d2515690957004cb
kalai02484 <kalai02484@gmail.com>
2025-11-07 16:35:59 +0530

    Overview Completed

---
+++ script.js
@@ -0,0 +1,61 @@
+var swiper = new Swiper(".blog-swiper", {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 4,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1024: {
+      slidesPerView: 6,
+    },
+    1280: {
+      slidesPerView: 6,
+    },
+  },
+});
+
+
+
+var swiper = new Swiper(".testimonial-swiper", {
+  effect: "coverflow",
+  centeredSlides: true,
+  spaceBetween: 30,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 0,
+    slideShadows: false
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    },
+    1560: {
+      slidesPerView: 3
+    }
+  }
+});
+
